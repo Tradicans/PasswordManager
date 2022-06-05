8199d1d1f182d35562fe51640faba4484f7b2948
Amber Rose <a_rose2@fanshaweonline.ca>
2022-06-05 18:03:40 -0400

    add account and validation working

--- PasswordManager/PasswordManager.cs
+++ PasswordManager/PasswordManager.cs
@@ -1,19 +1,15 @@
 ﻿/*
  * Program:         PasswordManager.exe
  * Module:          PasswordManager.cs
- * Date:            <enter a date>
- * Author:          <enter your name>
- * Description:     Some free starting code for INFO-3138 project 1, the Password Manager
- *                  application. All it does so far is demonstrate how to obtain the system date 
- *                  and how to use the PasswordTester class provided.
+ * Date:            2022-06-05
+ * Author:          Amber Rose
+ * Description:     Password Manager application
+ *                  for storage and retrieval of account data and passwords 
  */
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;            // File class
+using System.IO;                    // File class
 using Newtonsoft.Json;              // JsonConvert class
 using Newtonsoft.Json.Schema;       // JSchema class
 using Newtonsoft.Json.Linq;         // JObject class
@@ -29,106 +25,164 @@ namespace PasswordManager
 
         static void Main(string[] args)
         {
+            //UI
+            DateTime dateNow = DateTime.Now;
+            Console.WriteLine(dateNow.ToShortDateString() + "\t\t\t PASSWORD MANAGEMENT SYSTEM");
+            Console.WriteLine("****************************************************************************************************");
+
+
+            //read in schema file
             string json_schema;
             if (ReadFile(SCHEMA_FILE, out json_schema))
             {
+                //If the file doesn’t exist, creates an empty collection.
                 List<Account> accounts_list = new List<Account>();
                 string json_data;
-                if(ReadFile(ACCOUNTS_FILE, out json_data))
+                //a. If the application’s JSON file already exists,
+                if (ReadFile(ACCOUNTS_FILE, out json_data))
                 {
-                    //a. If the application’s JSON file already exists, reads this and deserializes it to create
-                    //a collection(such as a List<Account>) of account objects in memory.
+                    Console.WriteLine("\tAccount Entries:");
+                    //Console.WriteLine("****************************************************************************************************");
+                    //reads json data and deserializes it to create a collection of account objects in memory.
+                    accounts_list = JsonConvert.DeserializeObject<List<Account>>(json_data);
+                    //show existing accounts on menu
+                    int i = 0;
+                    foreach (Account a in accounts_list)
+                    {
+                        i++;
+                        Console.WriteLine("\t" + i + ".\t" + a.Description);
+
+                    }
+                    Console.WriteLine("****************************************************************************************************");
+                    //b.Shows the user a menu of the following command options:
+                    //i.List all fields for a selected account element
+                    Console.WriteLine("\tEnter # from the above list to select an entry");
                 }
 
 
+                Console.WriteLine("\tPress A to add an account");
+                Console.WriteLine("\tPress X to save and exit");
+                Console.WriteLine("****************************************************************************************************");
+                
+                bool notdone = true;
+                do
+                {
+                    Console.Write("\nEnter a command: ");
+                    //string pwText = Console.ReadLine();
+                    string command = Console.ReadLine();
+                    if(Int32.TryParse(command, out int i))
+                    {
+                        //TODO show details for accounts_list[i]
+
+                        //TODO show options to edit password or delete account
+
+
+                        //iii.Delete any account object from the collection.
+
+
+                        //iv.Change the password stored within a selected account element and simultaneously
+                        //update the StrengthNum, StrengthText and LastReset values
+                        //(again, this last value should be assigned the current system date)
+
+
+                        //c.If the user adds an account or modifies the password of an existing account,
+                        //the account object should be validated against your schema.If the new account
+                        //information fails the validation test, the user should validation informed that the
+                        //information entered is invalid and they should be prompted to re-input the required
+                        //information.
+
+
+                        //d.The updated collection of account objects should be written to the JSON file.
+                    }
+                    else
+                    {
+                        switch (command)
+                        {
+                            case "A":
+                                {
+                                    bool validAccount;
+                                    do
+                                    {
+                                    //ii.Add a new account object to the collection with the user providing values for
+                                    //all required fields, except the StrengthNum and the StrengthText fields
+                                    //(which will be assigned values using the PasswordTester class included with the
+                                    //starting code) and the LastReset field(which should be assigned the system date).
+                                    Account a = new Account();
+                                    string aEntry;
+                                    //account description
+                                    Console.Write("\n\nEnter the account name/description: ");
+                                    aEntry = Console.ReadLine();
+                                    a.Description = aEntry;
+                                    //user ID
+                                    Console.Write("\n\nEnter the user ID: ");
+                                    aEntry = Console.ReadLine();
+                                    a.UserId = aEntry;
+                                    //login webpage
+                                    Console.Write("\n\nEnter the login URL: ");
+                                    aEntry = Console.ReadLine();
+                                    a.LoginUrl = aEntry;
+                                    //account number
+                                    Console.Write("\n\nEnter the account number: ");
+                                    aEntry = Console.ReadLine();
+                                    a.AccountNum = aEntry;
+                                    //password
+                                    Password p = new Password();
+                                    a.Password = p;
+                                    Console.Write("\n\nEnter the password: ");
+                                    aEntry = Console.ReadLine();
+                                    p.Value = aEntry;
+                                    PasswordTester pw = new PasswordTester(aEntry);
+                                    p.StrengthText = pw.StrengthLabel;
+                                    p.StrengthNum = pw.StrengthPercent;
+                                    dateNow = DateTime.Now;
+                                    p.LastReset = dateNow;
+
+                                    //validate using schema
+                                    validAccount = ValidateAccount(a, json_schema);
+                                        if(validAccount)
+                                        {
+                                            //once valid, add to accounts list
+                                            accounts_list.Add(a);
+                                            Console.WriteLine("Account " + a.Description + " added");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Account information invalid. Please re-enter");
+                                        }
+                                        
+                                    } while (validAccount == false);
+                                    
+                                    
+                                    
+                                }
+                                break;
+                            case "X":
+                                {
+                                    notdone = false;
+                                    
+                                }
+                                break ;
+                            default:
+                                break;
+                        }
+                    }
+
+                    //Console.Write("\n\nEnter a command: ");
+                    //notdone = Console.ReadKey().KeyChar != 'X';
+
+                } while (notdone);
+                //d.The updated collection of account objects should be written to the JSON file.
+                foreach (var item in accounts_list)
+                {
+
+                }
+                //list accounts
             }
             else
             {
                 // Read operation for schema failed
                 Console.WriteLine("\nERROR:\tUnable to read the schema file.");
             }
-
-            {
-
-            }
-            //If the file doesn’t exist, creates an empty collection.
-
-
-
-            //UI
-            DateTime dateNow = DateTime.Now;
-            Console.WriteLine(dateNow.ToShortDateString() + "\t\t\t PASSWORD MANAGEMENT SYSTEM");
-            Console.WriteLine("****************************************************************************************************");
-            Console.WriteLine("\tAccount Entries:");
-            Console.WriteLine("****************************************************************************************************");
-
-
-
-
-
-
-
-
-            //b.Shows the user a menu of the following command options:
-
-
-            //i.List all fields for a selected account element
-
-
-            //ii.Add a new account object to the collection with the user providing values for
-            //all required fields, except the StrengthNum and the StrengthText fields
-            //(which will be assigned values using the PasswordTester class included with the
-            //starting code) and the LastReset field(which should be assigned the system date).
-
-
-            //iii.Delete any account object from the collection.
-
-
-            //iv.Change the password stored within a selected account element and simultaneously
-            //update the StrengthNum, StrengthText and LastReset values
-            //(again, this last value should be assigned the current system date)
-
-
-            //c.If the user adds an account or modifies the password of an existing account,
-            //the account object should be validated against your schema.If the new account
-            //information fails the validation test, the user should validation informed that the
-            //information entered is invalid and they should be prompted to re-input the required
-            //information.
-
-
-            //d.The updated collection of account objects should be written to the JSON file.
-
-
-
-
-
-
-            //// System date demonstration
-            //DateTime dateNow = DateTime.Now;
-            //Console.Write("PASSWORD MANAGEMENT SYSTEM (STARTING CODE), " + dateNow.ToShortDateString());
-
-            bool done;
-            do
-            {
-                Console.Write("\n\nEnter a password: ");
-                string pwText = Console.ReadLine();
-
-                try
-                {
-                    // PasswordTester class demonstration
-                    PasswordTester pw = new PasswordTester(pwText);
-                    Console.WriteLine("That password is " + pw.StrengthLabel);
-                    Console.WriteLine("That password has a strength of " + pw.StrengthPercent + "%");
-                }
-                catch (ArgumentException)
-                {
-                    Console.WriteLine("ERROR: Invalid password format");
-                }
-
-                Console.Write("\nTest another password? (y/n): ");
-                done = Console.ReadKey().KeyChar != 'y';
-
-            } while (!done);
 
             Console.WriteLine("\n");
         }//end main
@@ -137,8 +191,10 @@ namespace PasswordManager
         {
             try
             {
+                StreamReader r = new StreamReader(path);
                 // Read JSON file data 
-                json = File.ReadAllText(path);
+                //json = File.ReadAllText(path);
+                json = r.ReadToEnd();
                 return true;
             }
             catch
@@ -150,11 +206,11 @@ namespace PasswordManager
 
           // TODO Validates an account object against a schema (incomplete)
           //TODO move into account class
-        private static bool ValidateItem(Account account, string json_schema)
+        private static bool ValidateAccount(Account account, string json_schema)
         {
             // Convert account object to a JSON string 
             string json_data = JsonConvert.SerializeObject(account);
-
+            //JSchemaValidatingReader.Equals(json_schema, json_data);
             // Validate the data string against the schema contained in the 
             // json_schema parameter. Also, modify or replace the following 
             // return statement to return 'true' if item is valid, or 'false' 
@@ -163,7 +219,7 @@ namespace PasswordManager
             JObject itemObj = JObject.Parse(json_data);
             return itemObj.IsValid(schema);
 
-        } // end ValidateItem()
+        } // end ValidateAccount()
 
     } // end class Program
 
@@ -186,7 +242,6 @@ namespace PasswordManager
       //TODO move into own file
     class Password
     {
-        public string Description { get; set; }
         public string Value { get; set; }
         public int StrengthNum { get; set; }
         public string StrengthText { get; set; }
